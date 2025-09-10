331a1120a224b5bda9aff46ed539d074140eaa4c
Aryan Rajak <kalpanarajak09@gmail.com>
2025-09-10 20:20:15 +0530

    first commit

---
+++ class/lec10.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+int main()
+{
+             
+       for( int i=0; i<4;i++){
+        for(int p=0; p<=i; p++){
+            printf("*");
+        }
+        for(int j=3;j>i; j--){
+            printf(" ");
+        }
+        for(int k=3;k>=i; k--){
+            printf(" ");
+        }
+        for(int p=0; p<=i; p++){
+            printf("*");
+          }printf("\n");
+          
+}
+    for( int s=0; s<4;s++){
+        for(int a=4; a>s; a--){
+            printf("*");
+        }
+        for(int b=0;b<=s;b++){
+            printf(" ");
+        }
+         for(int b=1;b<=s;b++){
+            printf(" ");
+        }
+        
+        for(int c=4; c>s; c--){
+            printf("*");
+        }
+        printf("\n");
+    }}
+//     }}
+        
+       
+//           printf("\n");
+//         }
+
+
+// int n=5;
+//  for(int i=1;i<=n;i++)
+//  {
+//     for(int j=1;j<=n;j++){
+//         if(i==1 || i==n || j==1 || j==n){
+//             printf("*");
+//         } else{
+//             printf(" ");
+//         }
+//     } printf("\n");
+// }}
+   
+
+
+
+// int n=5;
+//  for(int i=1;i<=n;i++)
+//  {
+//     for(int j=1;j<=n;j++){
+//         if(i==1 || i==n || j==1 || j==n){
+//             printf("*");
+//         } else{
+//             printf(" ");
+//         }
+//     } printf("\n");
+   
+//  }
+
+
+
+    
---
+++ class/lec9.c
@@ -0,0 +1,61 @@
+ #include<stdio.h>
+int main(){
+   
+    int a=1;
+    for(int i=0;i<3;i++){
+        for(int j=0;j<=i;j++){
+            printf("%d" ,a++);
+        }
+        printf("\n");}
+    }
+
+
+    // int a=65;
+    // for(int i=0;i<3;i++){
+    //     for(int j=0;j<=i;j++){
+    //         printf("%c" ,a++);
+    //     }
+    //     printf("\n"); }
+
+
+
+    //    int a=65;
+    //    for( int i=0; i<4;i++){
+    //     for(int p=0; p<=i; p++){
+    //         printf(" ");
+    //     }
+    //     for(int j=3;j>i; j--){
+    //         printf("%c",a++);
+    //     }
+    //     for(int k=3;k>=i; k--){
+    //         printf("%c",a++);
+    //     }
+
+
+
+        // for(int k=0; k<=i; k++){
+        //     printf("%d",a++);
+        // }
+        //  for(int p=1; p<=i; p++){
+        //     printf("%d",a++);
+        // }
+
+
+
+
+    //      int a=65;
+    //    for( int i=0; i<4;i++){
+    //     for(int p=0; p<=i; p++){
+    //         printf(" ");
+    //     }
+    //     for(int j=3;j>i; j--){
+    //         printf("%c",a++);
+    //     }
+    //     for(int k=3;k>=i; k--){
+    //         printf("%c",a++);
+    //     }
+    //     printf("\n");
+        
+    //    }
+}
+    
